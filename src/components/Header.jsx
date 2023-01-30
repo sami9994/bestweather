3c98598602f170f7e45efc5a542d3943a24a51ad
sami9994 <samiomar994@gmail.com>
2023-01-30 11:05:00 +0300

    costumise header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import FontAwesomeIcon from '@fortawesome/fontawesome-free'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+
 import '../styles/css/Header.css'
 const Header = () => {
   return (
@@ -12,9 +13,10 @@ const Header = () => {
               type='text'
               placeholder='City'
               onChange={() => console.log('city search')}
+              className='input-search-field'
             />
             <button className='search-btn' type='submit'>
-              <FontAwesomeIcon icon='fa-sharp fa-solid fa-magnifying-glass-location' />
+              <SearchOutlinedIcon />
             </button>
           </form>
         </section>
