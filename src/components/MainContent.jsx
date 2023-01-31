ab6dd0bf7d2891411c82bedf00d238abba0d6353
sami9994 <samiomar994@gmail.com>
2023-01-31 16:56:10 +0300

    add card component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import '../styles/css/main-content.css'
+import Card from './Card'
 import Header from './header'
 const MainContent = () => {
   return (
     <div className='main-div'>
-      <section className='main-content'>main content</section>
+      <section className='main-content'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
     </div>
   )
 }
